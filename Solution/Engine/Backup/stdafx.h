94274a59cd6edb2861a501685ffcf99eb2b380a2
nian0601 <nian0601skola@hotmail.se>
2016-07-29 22:29:41 +0200

    First commit, copied everything from MagmaEngine

---
+++ Solution/Engine/Backup/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <CommonHelper.h>
+#include <DL_Debug.h>
+#include <Macros.h>
+#include "Engine.h"
+#include "GfxStructs.h"
+#include <Vector.h>
+#include <Matrix.h>
+
+#include "Entity/Include/CustomEntityTypes.h"
---
+++ Solution/Game/Game.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <IGame.h>
+#include <GrowingArray.h>
+#include <Entity/Include/World.h>
+
+#include <Subscriber.h>
+
+namespace Magma
+{
+	class AssetContainer;
+	class Camera;
+	class RendererProxy;
+}
+
+class Game : public Magma::IGame, public Subscriber
+{
+public:
+	Game();
+	~Game();
+
+	void Init(Magma::Engine& aEngine) override;
+	bool Update(float aDelta) override;
+	void OnResize(float aWidth, float aHeight) override;
+
+	void ReceiveMessage(const RenderMessage& aMessage);
+
+private:
+	void UpdateCamera(float aDelta);
+	void LoadLevel(Magma::AssetContainer& aAssetContainer);
+
+	Magma::Camera* myCamera;
+	Magma::RendererProxy* myRendererProxy;
+
+	Magma::World myWorld;
+};
+
